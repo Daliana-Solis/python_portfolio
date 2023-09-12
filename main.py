9ec35e12346bea077e416d9aa88915a348d0ad92
Daliana-Solis <dsolisso@uci.edu>
2023-09-12 18:48:07 -0500

    Added descriptions, images, and links

--- main.py
+++ main.py
@@ -24,15 +24,24 @@ with col2:
 conte2 = 'Below are some of the Python applications I have worked on.'
 st.write(conte2)
 
-col3, col4 = st.columns(2)
+col3, empty_coln, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=';')
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" +row['image'])
+
+        #later add your own URL
+        #f"[Source Code]({row['url']})"
+        st.write("[Source Code](https://pythonhow.com)")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" +row['image'])
+        st.write("[Source Code](https://pythonhow.com)")
 
